e58db140abd8a97e48f088170f096fdb6a6ce69f
PavelBulaienko <p380504942472@gmail.com>
2021-04-28 17:36:37 +0300

    add bootstrap components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,2 @@
 import './styles.scss';
-
-const refs = {
-  menuBtn: document.querySelector('.btnMenu'),
-  modal: document.querySelector('.modal'),
-  banner: document.querySelector('.banner'),
-  burgerMenuImg: document.querySelector('.burgerMenuImg'),
-  closeIcon: document.querySelector('.menuCloseIcon'),
-};
-
-refs.menuBtn.addEventListener('click', onMenuBtnClick);
-
-function onMenuBtnClick() {
-  refs.modal.classList.toggle('isHidden');
-  refs.banner.classList.toggle('darkBg');
-  refs.burgerMenuImg.classList.toggle('isHidden');
-  refs.closeIcon.classList.toggle('isHidden');
-}
+import bootstrap from 'bootstrap';
